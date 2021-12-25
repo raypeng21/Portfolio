68a2005c6f0acdfe91f7dd05b68e837c4ac33476
raypeng21 <62131265+raypeng21@users.noreply.github.com>
2021-12-25 02:33:40 -0600

    BJS Fundation Engineering project added

--- src/data.js
+++ src/data.js
@@ -8,6 +8,13 @@ export const featuredPortfolio = [
     },
     {
       id: 2,
+      title: "BJS Foundation Engineering",
+      img:
+        "https://imgur.com/DP7X1H8.png",
+        link: "http://www.bai-jia-song.com/",
+    },
+    {
+      id: 3,
       title: "Real Time Forum Project",
       img:
         "https://imgur.com/86pQGk4.png",
@@ -18,20 +25,27 @@ export const featuredPortfolio = [
   export const webPortfolio = [
     {
       id: 1,
-      title: "Bussiness",
+      title: "Local Bussiness",
       img:
         "https://i.imgur.com/wJ9PRT8.png",
         link: "https://www.raylacafe.ca/",
     },
     {
       id: 2,
+      title: "BJS Foundation Engineering",
+      img:
+        "https://imgur.com/DP7X1H8.png",
+        link: "http://www.bai-jia-song.com/",
+    },
+    {
+      id: 3,
       title: "Real Time Forum Project",
       img:
         "https://imgur.com/86pQGk4.png",
         link: "",
     },
     {
-      id: 3,
+      id: 4,
       title: "Mock Interview",
       img:
         "https://imgur.com/mFrMjJC.png",
